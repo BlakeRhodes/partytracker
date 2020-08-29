67489fc498aedf39f7daa476b3c83549ea52da13
Blake Rhodes <blakewrhodes@gmail.com>
2020-08-29 13:25:59 -0500

    move input to own card

--- src/app/character-list/character-list.component.ts
+++ src/app/character-list/character-list.component.ts
@@ -1,8 +1,8 @@
 import {Component, OnDestroy, OnInit} from '@angular/core';
-import {buildCharacter} from '../models/character';
+import {Character} from '../models/character';
 import {CharacterService} from '../services/character.service';
 import {Subscription} from 'rxjs';
-import {CdkDragDrop, moveItemInArray} from '@angular/cdk/drag-drop';
+import {FormBuilder, FormGroup} from '@angular/forms';
 
 @Component({
   selector: 'app-character-list',
@@ -12,12 +12,47 @@ import {CdkDragDrop, moveItemInArray} from '@angular/cdk/drag-drop';
 export class CharacterListComponent implements OnInit, OnDestroy {
   characters = [];
   subscription: Subscription;
+  form: FormGroup;
 
-  constructor(private characterService: CharacterService) {
+  constructor(
+    private characterService: CharacterService,
+    private formBuilder: FormBuilder,
+  ) {
   }
 
   ngOnInit(): void {
     this.subscription = this.characterService.getCharacters().subscribe(values => this.characters = values);
+    this.form = this.formBuilder.group({
+      name: [],
+      insight: [],
+      perception: [],
+      investigation: [],
+      str: [],
+      dex: [],
+      con: [],
+      int: [],
+      wis: [],
+      cha: [],
+      ac: [],
+    });
+  }
+
+  handleClick() {
+    this.characterService.add(
+      new Character({
+        name: this.name.value,
+        insight: this.insight.value + 10,
+        perception: this.perception.value + 10,
+        investigation: this.investigation.value + 10,
+        str: this.str.value,
+        dex: this.dex.value,
+        con: this.con.value,
+        int: this.int.value,
+        wis: this.wis.value,
+        cha: this.cha.value,
+        ac: this.ac.value,
+      })
+    );
   }
 
   ngOnDestroy(): void {
@@ -26,5 +61,49 @@ export class CharacterListComponent implements OnInit, OnDestroy {
 
   handleClosed(i: number) {
     this.characterService.remove(i);
+  }
+
+  get name() {
+    return this.form.get('name');
+  }
+
+  get insight() {
+    return this.form.get('insight');
+  }
+
+  get perception() {
+    return this.form.get('perception');
+  }
+
+  get investigation() {
+    return this.form.get('investigation');
+  }
+
+  get str() {
+    return this.form.get('str');
+  }
+
+  get dex() {
+    return this.form.get('dex');
+  }
+
+  get con() {
+    return this.form.get('con');
+  }
+
+  get int() {
+    return this.form.get('int');
+  }
+
+  get wis() {
+    return this.form.get('wis');
+  }
+
+  get cha() {
+    return this.form.get('cha');
+  }
+
+  get ac() {
+    return this.form.get('ac');
   }
 }
