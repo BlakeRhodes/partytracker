c3d71a688cb91b4a649fedd0097d4d2e02d3f8ea
Blake Rhodes <blakewrhodes@gmail.com>
2020-08-29 15:39:27 -0500

    move snackbar to bottom

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -3,5 +3,5 @@ import {MatSnackBarConfig} from '@angular/material/snack-bar';
 export const SNACKBAR_TIME = 2000;
 export const SNACKBAR_CONFIG =  {
   duration: SNACKBAR_TIME,
-  verticalPosition: 'top',
+  verticalPosition: 'bottom',
 } as MatSnackBarConfig<any>;
