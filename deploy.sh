1f846a85e0f3e1390686cfe2dc2474bc05b1d9a1
Blake Rhodes <blakewrhodes@gmail.com>
2020-08-30 15:00:52 -0500

    remove electron

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
 set -e
+
+ng test --watch=false
+ng lint --fix
+
 git add .
 git commit -m "$1"
 git push
-ng lint --fix
-ng test --browsers=Electron
+
 ng build --prod
 az storage blob upload-batch --account-name partytracker -s ./dist/party-tracker -d '$web'
--- karma.conf.js
+++ karma.conf.js
@@ -11,7 +11,6 @@ module.exports = function (config) {
       require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),
       require('@angular-devkit/build-angular/plugins/karma'),
-      require('karma-electron')
     ],
     client: {
       clearContext: false, // leave Jasmine Spec Runner output visible in browser
@@ -27,7 +26,7 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome', 'Electron'],
+    browsers: ['Chrome'],
     singleRun: false,
     restartOnFileChange: true
   });
