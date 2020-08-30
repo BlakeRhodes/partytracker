0c6f6e76307946a336c24072bf168c630bbd0479
Blake Rhodes <blakewrhodes@gmail.com>
2020-08-30 00:50:42 -0500

    test deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 set -e
-
+git add .
+git commit -m "$1"
+git push
 ng lint --fix
 ng build --prod
 az storage blob upload-batch --account-name partytracker -s ./dist/party-tracker -d '$web'
